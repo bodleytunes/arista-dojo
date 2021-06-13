6a697a37d43357e28f441f84e3c312b1ee4c37d9
Phill Jolliffe <phill@jolly.pub>
2021-06-13 21:53:50 +0100

    consolidated firewall policies

--- install-dojo.sh
+++ install-dojo.sh
@@ -122,7 +122,7 @@ else
     echo "WARNING: Any IP address / subnet can access the labs exposed port!!!!"
 fi
 
-gcloud compute --project=${GOOGLE_CLOUD_PROJECT} firewall-rules create default-allow-http \
+gcloud compute firewall-rules create eve-ng-telnet \
     --direction=INGRESS \
     --priority=1000 \
     --network=default \
@@ -131,9 +131,9 @@ gcloud compute --project=${GOOGLE_CLOUD_PROJECT} firewall-rules create default-a
     --source-ranges=$MY_IP_SUBNET \
     --target-tags=eve-ng
 
-gcloud compute --project=${GOOGLE_CLOUD_PROJECT} firewall-rules create eve-ng-telnet \
+gcloud compute firewall-rules update eve-ng-telnet \
     --direction=INGRESS \
-    --priority=1000 \
+    --priority=2000 \
     --network=default \
     --action=ALLOW \
     --rules=tcp:32769-33280 \
